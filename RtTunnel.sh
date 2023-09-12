b980f07fc2bf3e0345ee85275cfbd760c15d62de
69learn <144735591+69learn@users.noreply.github.com>
2023-09-12 21:02:02 +0330

    Add files via upload

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -100,6 +100,16 @@ clear
 echo "By --> 69 learn * Github.com/69learn * "
 echo "Your IP is: ($myip) "
 echo ""
+echo -e "\e[32m
+
+ ██████   █████      ██      ███████  █████  ██████  ███    ██ 
+██       ██   ██     ██      ██      ██   ██ ██   ██ ████   ██ 
+███████   ██████     ██      █████   ███████ ██████  ██ ██  ██ 
+██    ██      ██     ██      ██      ██   ██ ██   ██ ██  ██ ██ 
+ ██████   █████      ███████ ███████ ██   ██ ██   ██ ██   ████ 
+                                                                  
+\033[0m"
+echo -e "    \e[31mTelegram Channel: \e[34m@sixtininelearn\033[0m | \e[31mTelegram Group: \e[34m@sixtininelearn\033[0m\n"
 echo " --------#- Reverse Tls Tunnel (RTT tunnel) -#--------"
 echo "1) Install"
 echo "2) Uninstall"
